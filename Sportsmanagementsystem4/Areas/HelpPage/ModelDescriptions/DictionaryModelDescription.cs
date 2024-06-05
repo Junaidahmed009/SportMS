ff48607586a5dafe5c832a57cf7ba28231b31c84
Junaidahmed009 <junaidraja09009@gmail.com>
2024-06-06 02:46:21 +0500

    first commit

---
+++ Sportsmanagementsystem4/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sportsmanagementsystem4.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Sportsmanagementsystem4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sportsmanagementsystem4.WebApiApplication" Language="C#" %>
